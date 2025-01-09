d975187eea747c73951acea402e7f15eefc01bb9
juliandellaguardia@hotmail.com <juliandellaguardia@hotmail.com>
2025-01-09 23:08:50 +0100

    made getting address more reliable

--- radio_libraries/address.c
+++ radio_libraries/address.c
@@ -1,7 +1,10 @@
 /*Julian Della Guardia*/
 
+#define F_CPU 32000000UL
+
 #include "address.h"
 #include <avr/io.h>
+#include <util/delay.h>
 
 uint8_t getSensorAddress(void){
     // Configure all pins of Port B as input
@@ -16,6 +19,8 @@ uint8_t getSensorAddress(void){
     PORTB.PIN5CTRL = PORT_OPC_PULLUP_gc;
     PORTB.PIN6CTRL = PORT_OPC_PULLUP_gc;
     PORTB.PIN7CTRL = PORT_OPC_PULLUP_gc;
+
+    _delay_ms(10);
 
     // Read the value of Port B input pins
     return ~PORTB.IN;
